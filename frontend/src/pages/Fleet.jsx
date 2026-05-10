f028c9f95b37e05c024983885863541613208e7d
mohamedamine <mamounsafta58@gmail.com>
2026-05-11 01:32:15 +0200

    Fix FiClock import in Fleet page

--- frontend/src/pages/Fleet.jsx
+++ frontend/src/pages/Fleet.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import api from '../api';
-import { FiPlus, FiTruck, FiTool, FiActivity, FiSearch, FiAlertTriangle, FiCheckCircle, FiShield, FiX, FiEdit, FiTrash2 } from 'react-icons/fi';
+import { FiPlus, FiTruck, FiTool, FiActivity, FiSearch, FiAlertTriangle, FiCheckCircle, FiShield, FiX, FiEdit, FiTrash2, FiClock } from 'react-icons/fi';
 import { useLanguage } from '../LanguageContext';
 import { useNotifications } from '../NotificationsContext';
 

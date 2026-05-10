a4b3bc57c7d879965ccb44465bb5a51b47c5906d
mohamedamine <mamounsafta58@gmail.com>
2026-05-10 16:55:37 +0200

    Fix SSL connection

--- backend/database.js
+++ backend/database.js
@@ -4,9 +4,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required for Supabase/Render connections
-  }
+  ssl: true
 });
 
 // Helper to convert SQLite "?" syntax to PostgreSQL "$1, $2" syntax
